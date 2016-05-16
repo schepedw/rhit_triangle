3a333d7d81d91e49c6bc8c7a5961c7973fc91e01
Dan Schepers <dan.schepers@dschepers-m1.local>
2016-05-15 19:12:59 -0500

    set up twitter

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,7 @@
 TwitterProxy = Twitter::REST::Client.new do |config|
-  config.consumer_key        = '1NtXXpaLM0uKj5mvih4pfdzTp'
-  config.consumer_secret     = 'Jf9s3vu6Ou32YgT5noapnqeAZJdk3cSU6olvUsuuCEnK8HYA2O'#TODO: fix this
-  config.access_token        = "YOUR_ACCESS_TOKEN"
-  config.access_token_secret = "YOUR_ACCESS_SECRET"
+  config.consumer_key        = AppConfig.twitter_creds.consumer_key
+  config.consumer_secret     = AppConfig.twitter_creds.consumer_secret
+  config.access_token        = AppConfig.twitter_creds.access_token
+  config.access_token_secret = AppConfig.twitter_creds.access_token_secret
 end
+TwitterProxy.user('RHIT_Triangle')
