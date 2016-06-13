f12726f734b1d90b45dbdf47fa8b15bc04633ff8
Dan Schepers <dan.schepers@dschepers-m1.local>
2016-06-12 20:45:16 -0500

    hotfix for role

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,7 +1,7 @@
 class Role < ActiveRecord::Base
   has_and_belongs_to_many :members, join_table: :members_roles
 
-  belongs_to :resource, polymorphic: true, optional: true
+  belongs_to :resource, polymorphic: true, required: false
 
   validates :resource_type, inclusion: { in: Rolify.resource_types }, allow_nil: true
 
