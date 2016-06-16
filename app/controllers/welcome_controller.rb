48d6cfdd349643920224d822fe81fe6b1757f6b3
Dan Schepers <dan.schepers@dschepers-m1.local>
2016-06-15 21:52:35 -0500

    turn landing hero to a carousel

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,9 @@
 class WelcomeController < ApplicationController
   def index
     @tweets = TwitterProxy.user_timeline[0..4]
+  rescue Twitter::Error
+    @tweets = [] #TODO - make a pseudo object that says 'error' or smn
+  ensure
     @officers = AppConfig.officers
   end
 end
