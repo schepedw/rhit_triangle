99db2b26bb66a3d9fdd3e8d5f5e96c2f1b60b35a
Dan Schepers <dan.schepers@dschepers-m1.local>
2016-07-19 22:37:39 -0500

    seed pictures; need to scp them to prod server. Need to get rid of carousel functionality if only one pic available

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,11 +1,6 @@
 class DonationsController < ApplicationController
   def index
-    @projects = [
-      OpenStruct.new(title: Faker::Company.catch_phrase, description: Faker::Lorem.paragraph, capital_progress: 1832.12, total_cost: 29918.12, pictures: images ),
-      OpenStruct.new(title: Faker::Company.catch_phrase, description: Faker::Lorem.paragraph, capital_progress: 32.12, total_cost: 218.12, pictures: images),
-      OpenStruct.new(title: Faker::Company.catch_phrase, description: Faker::Lorem.paragraph, capital_progress: 182.12, total_cost: 2918.12, pictures: images),
-      OpenStruct.new(title: Faker::Company.catch_phrase, description: Faker::Lorem.paragraph, capital_progress: 0.00, total_cost: 29918.12, pictures: images)
-    ]
+    @projects = Project.where.not(price: 0)
   end
 
   private
