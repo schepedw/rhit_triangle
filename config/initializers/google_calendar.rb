53e9fb62360c49e697bae339668553fdaf2381a4
Dan Schepers <dan.schepers@dschepers-m1.local>
2016-09-15 20:48:38 -0500

    stub external services in test

--- config/initializers/google_calendar.rb
+++ config/initializers/google_calendar.rb
@@ -2,6 +2,8 @@ require 'google/apis/calendar_v3'
 require 'googleauth'
 
 Gcal = Google::Apis::CalendarV3::CalendarService.new
-json_key_io = File.open('./service_account_creds.json')
-Gcal.authorization =
-  Google::Auth::ServiceAccountCredentials.make_creds({scope: Google::Apis::CalendarV3::AUTH_CALENDAR, json_key_io: json_key_io})
+if Rails.env != 'test'
+  json_key_io = File.open('./service_account_creds.json')
+  Gcal.authorization =
+    Google::Auth::ServiceAccountCredentials.make_creds({scope: Google::Apis::CalendarV3::AUTH_CALENDAR, json_key_io: json_key_io})
+end
