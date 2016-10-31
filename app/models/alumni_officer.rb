0a08190b95c2a504a8bde85c235581396e03749a
Dan Schepers <dan.schepers@dschepers-m1.local>
2016-10-30 21:12:26 -0500

    rm unneeded officer logic & files

--- app/models/alumni_officer.rb
+++
@@ -1,5 +0,0 @@
-# TODO: rm after release of https://github.com/schepedw/rhit_triangle/pull/13
-class AlumniOfficer < ActiveRecord::Base
-  belongs_to :member
-  default_scope { includes(:member) }
-end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,7 +1,7 @@
 class Role < ActiveRecord::Base
   belongs_to :member
 
-  validates :role_type, inclusion: { in: %w[alumni active] }, allow_nil: true
+  validates :role_type, inclusion: { in: %w[alumni active admin] }, allow_nil: true
 
   def self.sort(roles)
     roles.sort_by(&:sort_val)
